1c834173abed32cdebed26843e3880dd0a63dccf
LuisM000 <luis.m.r@outlook.com>
2012-09-04 17:25:30 +0200

    Modificaciones por error en carpetas

---
+++ FuncionesTratamientoImagenes/ApoloMetro/ApoloMetro/ApoloMetro/ApoloMetro/Cargando.vb
@@ -0,0 +1,3 @@
+﻿Public Class Cargando
+
+End Class
---
+++ FuncionesTratamientoImagenes/ApoloMetro/ApoloMetro/ApoloMetro/ApoloMetro/Eleccion.vb
@@ -0,0 +1,138 @@
+﻿Public Class Eleccion
+
+    Private Sub PictureBox3_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox3.MouseEnter
+        PictureBox3.ImageLocation = "imagenes\white\back.png"
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PictureBox3_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox3.MouseLeave
+        PictureBox3.ImageLocation = "imagenes\black\back.png"
+        Me.Cursor = Cursors.Default
+    End Sub
+    
+    Private Sub PictureBox1_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox1.MouseEnter
+        PictureBox1.ImageLocation = "imagenes\botones\efectoWhite.png"
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PictureBox1_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox1.MouseLeave
+        PictureBox1.ImageLocation = "imagenes\botones\efectoBlack.png"
+        Me.Cursor = Cursors.Default
+    End Sub
+
+
+    Private Sub PictureBox2_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox2.MouseEnter
+        PictureBox2.ImageLocation = "imagenes\botones\TrasformacionWhite.png"
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PictureBox2_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox2.MouseLeave
+        PictureBox2.ImageLocation = "imagenes\botones\TrasformacionBlack.png"
+        Me.Cursor = Cursors.Default
+    End Sub
+    Private Sub PictureBox4_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox4.MouseEnter
+        PictureBox4.ImageLocation = "imagenes\botones\redimensionarWhite.png"
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PictureBox4_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox4.MouseLeave
+        PictureBox4.ImageLocation = "imagenes\botones\redimensionarBlack.png"
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub PictureBox5_MouseEnter(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox5.MouseEnter
+        PictureBox5.ImageLocation = "imagenes\botones\FormatoWhite.png"
+        Me.Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub PictureBox5_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox5.MouseLeave
+        PictureBox5.ImageLocation = "imagenes\botones\FormatoBlack.png"
+        Me.Cursor = Cursors.Default
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        Timer3.Enabled = True
+    End Sub
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox4.Click
+        Timer4.Enabled = True
+    End Sub
+
+
+    Private Sub PictureBox2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox2.Click
+        Timer5.Enabled = True
+    End Sub
+
+    Private Sub Eleccion_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Size = New Point(69, 401)
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub PictureBox5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox5.Click
+        Timer6.Enabled = True
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If Me.Width > 2 Then
+            Me.Width = Me.Width - 10
+        Else
+            Me.Close()
+            Timer1.Enabled = False
+            Form1.Visible = True
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        If Me.Width < 231 Then
+            Me.Width = Me.Width + 10
+        Else
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer3.Tick
+        If Me.Width > 2 Then
+            Me.Width = Me.Width - 10
+        Else
+            Me.Close()
+            Timer3.Enabled = False
+            Efectos.Show()
+        End If
+    End Sub
+
+ 
+    Private Sub Timer4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer4.Tick
+        If Me.Width > 2 Then
+            Me.Width = Me.Width - 10
+        Else
+            Me.Close()
+            Timer4.Enabled = False
+            Redimensionar.Show()
+        End If
+    End Sub
+
+    Private Sub Timer5_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer5.Tick
+        If Me.Width > 2 Then
+            Me.Width = Me.Width - 10
+        Else
+            Me.Close()
+            Timer5.Enabled = False
+            Afin.Show()
+        End If
+    End Sub
+
+
+    Private Sub Timer6_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer6.Tick
+        If Me.Width > 2 Then
+            Me.Width = Me.Width - 10
+        Else
+            Me.Close()
+            Timer6.Enabled = False
+            Formato.Show()
+        End If
+    End Sub
+End Class
