5ec5b7de1ff2ce58c996749c26a91024d8584bdc
LuisM000 <luis.m.r@outlook.com>
2012-09-05 12:29:52 +0200

    Eliminadas carpetas en branch master

--- FuncionesTratamientoImagenes/ApoloV0.9/Canvas0.9/Canvas0.9/bin/FOrmulariosSecundarios/FormularioSec/Cargando.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Cargando
-
-    Private Sub Cargando_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        ControlBox = False
-    End Sub
-End Class
--- FuncionesTratamientoImagenes/ApoloV0.9/Canvas0.9/Canvas0.9/bin/FOrmulariosSecundarios/FormularioSec/PropiedadesImagen.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class PropiedadesImagen
-
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        Me.Close()
-    End Sub
-End Class
