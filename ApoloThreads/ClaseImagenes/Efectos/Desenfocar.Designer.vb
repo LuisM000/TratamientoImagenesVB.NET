b6d8ef57b61fab43c058b2539d591091cdb38908
LuisM000 <luis.m.r@outlook.com>
2012-12-07 12:45:34 +0100

    desenfoque anadido

---
+++ ApoloThreads/ClaseImagenes/Efectos/Desenfocar.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Desenfocar
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.HScrollBar2 = New System.Windows.Forms.HScrollBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(188, 160)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 30)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Cerrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(279, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 30)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Aplicar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Desenfoque horizontal"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(289, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(19, 46)
+        Me.HScrollBar1.Maximum = 255
+        Me.HScrollBar1.Minimum = -254
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(242, 20)
+        Me.HScrollBar1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 15)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Desenfoque vertical"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(289, 113)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Label4"
+        '
+        'HScrollBar2
+        '
+        Me.HScrollBar2.Location = New System.Drawing.Point(19, 110)
+        Me.HScrollBar2.Maximum = 255
+        Me.HScrollBar2.Minimum = -254
+        Me.HScrollBar2.Name = "HScrollBar2"
+        Me.HScrollBar2.Size = New System.Drawing.Size(242, 20)
+        Me.HScrollBar2.TabIndex = 10
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(19, 160)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 30)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Restablecer"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Desenfocar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 202)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.HScrollBar2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Desenfocar"
+        Me.Text = "Desenfocar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents HScrollBar1 As System.Windows.Forms.HScrollBar
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents HScrollBar2 As System.Windows.Forms.HScrollBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+End Class
---
+++ ApoloThreads/ClaseImagenes/Efectos/Desenfocar.vb
@@ -0,0 +1,47 @@
+﻿Imports ClaseImagenes.Apolo
+Public Class Desenfocar
+
+    Dim objetoTratamiento As New TratamientoImagenes 'Instancia a la clase TratamientoImagenes
+    Dim bmpP As New Bitmap(Principal.PictureBox1.Image) 'Imagen de principal
+
+    Private Sub Desenfocar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HScrollBar1.Maximum = bmpP.Width 'Asignamos el máximo y mínimo como el ancho de la imagen
+        HScrollBar1.Minimum = -bmpP.Width
+        HScrollBar2.Maximum = bmpP.Height 'Asignamos el máximo y mínimo como el alto de la imagen
+        HScrollBar2.Minimum = -bmpP.Height
+        HScrollBar1.Value = 0
+        Label1.Text = HScrollBar1.Value
+        HScrollBar2.Value = 0
+        Label4.Text = HScrollBar1.Value
+        'Asignamos el gestor que controle cuando sale imagen
+        AddHandler objetoTratamiento.actualizaBMP, New ActualizamosImagen(AddressOf Principal.actualizarPicture)
+        'Asignamos el gestor que controle cuando se abre una imagen nueva
+        AddHandler objetoTratamiento.actualizaNombreImagen, New ActualizamosNombreImagen(AddressOf Principal.actualizarNombrePicture)
+
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        Label1.Text = HScrollBar1.Value
+    End Sub
+
+    Private Sub HScrollBar2_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar2.Scroll
+        Label4.Text = HScrollBar2.Value
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        HScrollBar1.Value = 0
+        HScrollBar2.Value = 0
+        Label1.Text = 0
+        Label4.Text = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If BackgroundWorker1.IsBusy = False Then
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Principal.PictureBox1.Image = objetoTratamiento.desenfoque(bmpP, -HScrollBar1.Value, -HScrollBar2.Value)
+    End Sub
+End Class
