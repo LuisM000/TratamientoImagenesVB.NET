da2347f52ad312030848b1f00a8ca45f66cba0cb
LuisM000 <luis.m.r@outlook.com>
2013-06-29 13:35:16 +0200

    Incluida aplicacion MDI y WebCam (sOlo .exe)

---
+++ ApoloThreads/ClaseImagenes/LockBits-Scan0/InicioLockBits.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InicioLockBits
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InicioLockBits))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(154, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(157, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Tratamiento de imágenes"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(202, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "WebCam"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(210, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cerrar"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.TextBox1.Location = New System.Drawing.Point(11, 104)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(441, 52)
+        Me.TextBox1.TabIndex = 5
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(173, 120)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(154, 23)
+        Me.TextBox2.TabIndex = 4
+        '
+        'InicioLockBits
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(464, 168)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "InicioLockBits"
+        Me.Opacity = 0.9R
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LockBits/Scan0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+End Class
---
+++ ApoloThreads/ClaseImagenes/LockBits-Scan0/InicioLockBits.vb
@@ -0,0 +1,45 @@
+﻿Public Class InicioLockBits
+
+    Private Sub InicioLockBits_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each c As Object In Me.Controls
+            If c.GetType Is GetType(Label) Then
+                AddHandler DirectCast(c, Label).MouseEnter, AddressOf dentroMouse
+                AddHandler DirectCast(c, Label).MouseLeave, AddressOf fueraMouse
+            End If
+        Next
+    End Sub
+    Sub dentroMouse(ByVal sender As Object, ByVal e As System.EventArgs)
+        Me.Cursor = Cursors.Hand
+        DirectCast(sender, Label).Font = New Font(Label1.Font, FontStyle.Bold)
+    End Sub
+    Sub fueraMouse(ByVal sender As Object, ByVal e As System.EventArgs)
+        Me.Cursor = Cursors.Default
+        DirectCast(sender, Label).Font = New Font(Label1.Font, FontStyle.Regular)
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Dim Process1 As New Process
+        Process1.StartInfo.RedirectStandardOutput = True
+        Process1.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory().ToString & "\SubprogramasApolo\ImageMDI.exe"
+        Process1.StartInfo.UseShellExecute = False
+        Process1.StartInfo.CreateNoWindow = True
+        Process1.Start()
+        'A la espera de que se cierre...
+        Process1.WaitForExit()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim Process1 As New Process
+        Process1.StartInfo.RedirectStandardOutput = True
+        Process1.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory().ToString & "\SubprogramasApolo\WebCam.exe"
+        Process1.StartInfo.UseShellExecute = False
+        Process1.StartInfo.CreateNoWindow = True
+        Process1.Start()
+        'A la espera de que se cierre...
+        Process1.WaitForExit()
+    End Sub
+End Class
